24810ec4d115a3f3f0d8a6e8a5a565e7163c7a45
bhanukosuri <61044101+bhanukosuri@users.noreply.github.com>
2023-08-14 12:19:39 +0530

    After Implementing Sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,23 +4,25 @@ const db = require('../util/database')
 
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
         path: '/products'
-      });
+      })
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+      console.log(err)
+    })
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then(([product]) => {
+    .then(product => {
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products'
       });
@@ -29,10 +31,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/index', {
-        prods: rows,
+        prods: products,
         pageTitle: 'Shop',
         path: '/'
       })
--- util/database.js
+++ util/database.js
@@ -1,10 +1,5 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '8500878111'
-})
+const sequelize = new Sequelize('node-complete', 'root', '8500878111', { dialect: 'mysql', host: 'localhost' })
 
-module.exports = pool.promise()
+module.exports = sequelize
